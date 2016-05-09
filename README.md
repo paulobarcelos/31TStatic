9c64d5fd41f05db02cc5d9df8a59f0790fd103d2
Paulo Barcelos <paulo@azucrina.org>
2016-05-09 09:58:43 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,128 @@
+# 31T Static
+
+## Dependecies
+- Nodejs
+- Npm
+- Bower
+- Jekyll
+
+## Setup
+```
+git clone https://github.com/oakwood/31TStatic.git
+cd 31TStatic
+npm install
+bower install
+```
+## Distribution Setups
+There are different distribution "setups". All setups have the same content, but each one have a difference in how the content get piped before being deployed to the final distribution directory.
+- `dev`
+- `polymer`
+- `gzip`
+
+#### `dev` distribution
+It's the content of `/src` compiled with Jekyll.
+
+The final content get's deployed to `/dist_dev`.
+
+#### `polymer` distribution
+Same as `dev`, but the content also needs to pass through a JSLint test first, and afterwards, the main Polymer resources (`/src/assets/elements/elements.html`) gets "polybuilt" to `/src/assets/elements/elements.build.html` and `/src/assets/elements/elements.build.js`. Service worker elements also get placed in the correct paths, and paths are updated to use minified resources.
+
+The final content get's deployed to `/dist_polymer`.
+
+#### `gzip` distribution
+Same as `polymer`, but the content is gzipped afterwards.
+
+The final content get's deployed to `/dist_gzip`.
+
+## Command line interface
+Every distribution setup respond to the same set of command line tasks:
+- `build`
+- `serve`
+- `watch`
+- `clean`
+
+#### `npm run gulp -- build:{distribution}` task
+Builds the content of the `/src` to one of the distribution directories.
+- `npm run gulp -- build:dev` (builds to `/dist`)
+- `npm run gulp -- build:polymer` (builds to `/dist_polymer`)
+- `npm run gulp -- build:gzip` (builds to `/dist_gzip`)
+
+#### `npm run gulp -- serve:{distribution}` task
+Serves the content of the distribution directory.
+- `npm run gulp -- serve:dev` (serves at http://localhost:5500)
+- `npm run gulp -- serve:polymer` (serves at http://localhost:5501)
+- `npm run gulp -- serve:gzip` (serves at http://localhost:5502)
+
+#### `npm run gulp -- watch:{distribution}` task
+Serves the content of the distribution directory and rebuilds every time there is a file change.
+
+#### `npm run gulp -- clean:{distribution}` task
+Cleans the content of the distribution directory.
+
+## Environment flags
+All the command line task accepts a `--environment` flag. This flag will be used internally by Jerkyll to enable/disable features and also to control environment specific settings (@see `/src/_config.yml`).
+
+There are 3 possible values for the flags
+- `--environment=development` (default)
+- `--environment=production`
+- `--environment=stage`
+
+Sample usage
+```
+npm run gulp -- build:dev --environment=production
+npm run gulp -- serve:polymer --environment=stage
+
+# no flag...
+npm run gulp -- watch:gzip
+# ...same as:
+npm run gulp -- watch:gzip --environment=development
+```
+## Deploying
+To deploy the `gzip` setup to Amazon S3, please create the corresponding configuration
+files in `/aws-config/[environment].json`.
+For security, those files should not be included on the repository.
+
+Examples:
+
+### `/aws-config/stage.json`
+
+```
+{
+  "key": "YOUR_S3_KEY",
+  "secret": "YOUR_S3_SECRET",
+  "bucket": "yourbucket-stage.com",
+  "region": "us-east-1"
+}
+
+```
+### `/aws-config/production.json`
+
+```
+{
+  "key": "YOUR_S3_KEY",
+  "secret": "YOUR_S3_SECRET",
+  "bucket": "yourbucket.com",
+  "region": "us-east-1"
+}
+
+```
+
+When you are ready to deploy, run:
+```
+npm run gulp -- deploy --environment=stage
+```
+or
+```
+npm run gulp -- deploy --environment=production
+```
+## Updating dependencies
+
+### Bower
+The bower dependencies are intentionally locked to avoid surprises, but it's recommended that you keep them updated - you will just have to do it manually, one by one.
+
+In order to make your life easier, use `npm-check-updates`.
+
+```
+npm install -g npm-check-updates
+ncu -m bower
+```
---
+++ gulpfile.js
@@ -0,0 +1,418 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var glob = require('glob');
+var polybuild = require('polybuild');
+var browserSync = require('browser-sync');
+var argv = require('yargs').default('environment', 'development').argv;
+var reload = browserSync.reload;
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var merge = require('merge-stream');
+var runSequence = require('run-sequence');
+var streamqueue = require('streamqueue')
+var del = require('del');
+
+var historyApiFallback = require('connect-history-api-fallback');
+
+var SRC = 'src';
+var DEST_DEV = 'dist_dev';
+var DEST_POLYMER = 'dist_polymer';
+var DEST_GZIP = 'dist_gzip';
+
+var AUTOPREFIXER_BROWSERS = [
+	'ie >= 10',
+	'ie_mob >= 10',
+	'ff >= 30',
+	'chrome >= 34',
+	'safari >= 7',
+	'opera >= 23',
+	'ios >= 7',
+	'android >= 4.4',
+	'bb >= 10'
+];
+
+
+/**
+ * Lint JavaScript fom SRC
+ */
+gulp.task('jshint', function () {
+	return gulp.src([
+		SRC + '/assets/elements/**/*.js',
+		SRC + '/assets/elements/**/*.html',
+	])
+	.pipe(reload({stream: true, once: true}))
+	.pipe($.jshint.extract()) // Extract JS from .html files
+	.pipe($.jshint())
+	.pipe($.jshint.reporter('jshint-stylish'))
+	.pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
+/**
+ * Lint JavaScript fom SRC (this won't fail, so it's good for the dev environment)
+ */
+gulp.task('soft-jshint', function () {
+	return gulp.src([
+		SRC + '/assets/elements/**/*.js',
+		SRC + '/assets/elements/**/*.html',
+	])
+	.pipe($.jshint.extract()) // Extract JS from .html files
+	.pipe($.jshint())
+	.pipe($.jshint.reporter('jshint-stylish'))
+});
+
+/**
+ * Runs Jekyll using --source SRC --destination DEST_DEV
+ */
+gulp.task('jekyll', function (gulpCallBack){
+	var environment;
+	switch (argv.environment) {
+		case 'production':
+			environment = argv.environment;
+			break;
+		case 'stage':
+			environment = argv.environment;
+			break;
+		default:
+			environment = 'development';
+
+	}
+	var spawn = require('child_process').spawn;
+	var jekyll = spawn(
+		'jekyll', ['build', '--source', SRC, '--destination', DEST_DEV],
+		{
+			stdio: 'inherit',
+			env: {
+				PATH: process.env.PATH,
+				JEKYLL_ENV: environment
+			}
+		}
+	);
+	jekyll.on('exit', function(code) {
+		gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+	});
+});
+
+gulp.task('js', function() {
+  return gulp.src([DEST_DEV + '/**/*.{js,html}', '!'+DEST_DEV+'/assets/bower_components/**/*'])
+	// Extract JS from .html files
+	.pipe($.if('*.html', $.crisper({scriptInHead:false})))
+	.pipe($.sourcemaps.init())
+	.pipe($.if('*.js', $.babel({
+		presets: ['es2015']
+	})))
+	.pipe($.sourcemaps.write())
+	.pipe(gulp.dest('.tmp/'))
+	.pipe(gulp.dest(DEST_DEV));
+});
+
+/**
+ * Copies prepare files for polymer, from DEST_DEV to DEST_POLYMER
+ */
+gulp.task('polymer-copy', function () {
+	var app = gulp.src([ DEST_DEV + '/**/*'])
+	.pipe(gulp.dest(DEST_POLYMER));
+
+	var swBootstrap = gulp.src([DEST_DEV + '/assets/bower_components/platinum-sw/bootstrap/*.js'])
+	.pipe(gulp.dest(DEST_POLYMER + '/assets/elements/bootstrap'));
+
+	var swToolbox = gulp.src([DEST_DEV + '/assets/bower_components/sw-toolbox/*.js'])
+	.pipe(gulp.dest(DEST_POLYMER + '/sw-toolbox'));
+
+	return merge(app, swBootstrap, swToolbox)
+	.pipe($.size({title: 'copyBuild'}));
+});
+/**
+ * Replaces paths DEST_POLYMER so they load the minified resources
+ */
+gulp.task('polymer-use-minified-paths', function () {
+	return gulp.src([
+		DEST_DEV + '/**/*.html'
+	])
+	// Replace path for vulcanized assets
+	.pipe($.replace('elements/elements.html', 'elements/elements.build.html'))
+	// Replace path for minimized webcomponentsjs
+	.pipe($.replace('bower_components/webcomponentsjs/webcomponents-lite.js', 'bower_components/webcomponentsjs/webcomponents-lite.min.js'))
+	.pipe($.replace('bower_components/webcomponentsjs/webcomponents.js', 'bower_components/webcomponentsjs/webcomponents.min.js'))
+	// Minify HTML
+	.pipe($.if('*.html', $.minifyHtml()))
+	// Output Files
+	.pipe(gulp.dest(DEST_POLYMER))
+	.pipe($.size({title: 'polymer-minify'}));
+});
+/**
+ * Polybuild
+ */
+gulp.task('polymer-polybuild', function () {
+	return gulp.src(DEST_POLYMER + '/assets/elements/elements.html')
+	.pipe(polybuild({maximumCrush: true}))
+	.pipe(gulp.dest(DEST_POLYMER + '/assets/elements/'))
+	.pipe($.size({title: 'polymer-polybuild'}));
+});
+/**
+ * Deletes unused resources
+ */
+gulp.task('polymer-clean',
+	del.bind(null, [
+		DEST_POLYMER + '/assets/test',
+
+		DEST_POLYMER + '/assets/bower_components/**/*',
+
+		'!' + DEST_POLYMER + '/assets/bower_components/webcomponentsjs',
+			  DEST_POLYMER + '/assets/bower_components/webcomponentsjs/**/*',
+		'!' + DEST_POLYMER + '/assets/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+		'!' + DEST_POLYMER + '/assets/bower_components/webcomponentsjs/webcomponents.min.js',
+
+			  DEST_POLYMER + '/assets/elements/**/*',
+		'!' + DEST_POLYMER + '/assets/elements/elements.build.html',
+		'!' + DEST_POLYMER + '/assets/elements/elements.build.js'
+	]
+));
+/**
+ * Add a revison query paramenter to the resources. Based on manifest version.
+ */
+gulp.task('polymer-revision', function () {
+	var fs = require('fs');
+	var manifest = JSON.parse(fs.readFileSync(DEST_POLYMER + '/manifest.json'));
+	return gulp.src([DEST_POLYMER + '/**/*.html'])
+		.pipe($.replace('elements.build.html', 'elements.build.html?'+manifest.version))
+		.pipe($.replace('elements.build.js', 'elements.build.js?'+manifest.version))
+		.pipe(gulp.dest(DEST_POLYMER))
+		.pipe($.size({title: 'revision'}));
+});
+
+
+/**
+ * Babelify
+ */
+gulp.task('babel', function () {
+	var app = gulp.src([ DEST_DEV + '/assets/elements/elements.build.js'])
+	.pipe($.babel({
+		presets: ['es2015']
+	}))
+	.pipe(gulp.dest(DEST_DEV));
+
+	return merge(app)
+	.pipe($.size({title: 'babel'}));
+});
+/**
+ * Clones the content from DEST_POLYMER to DEST_GZIP
+ */
+gulp.task('gzip-clone', function () {
+	return gulp.src([ DEST_POLYMER + '/**/*'])
+	.pipe(gulp.dest(DEST_GZIP))
+	.pipe($.size({title: 'gzip-clone'}));
+});
+/**
+ * Clones the content from DEST_POLYMER to DEST_GZIP and gzipps it
+ */
+gulp.task('gzip-compression', ['gzip-clone'], function () {
+	return gulp.src([
+		DEST_GZIP + '/**/*.html',
+		DEST_GZIP + '/**/*.js',
+		DEST_GZIP + '/**/*.css',
+		DEST_GZIP + '/**/*.json',
+		DEST_GZIP + '/**/*.svg',
+		DEST_GZIP + '/**/*.woff2'
+	])
+	.pipe($.gzip({ append: false }))
+	.pipe(gulp.dest(DEST_GZIP))
+	.pipe($.size({title: 'gzip-compression'}));
+});
+
+/**
+ * Builds the Gzip setup and publish to s3
+ */
+gulp.task('s3', ['build:gzip'], function () {
+	var aws = JSON.parse(fs.readFileSync('./aws-config/'+argv.environment+'.json'));
+
+	var cacheControl = 'max-age=4320000, no-transform, public';
+
+	return merge (
+		gulp.src([ DEST_GZIP + '/**/*.html' ])
+		.pipe($.s3(aws, {
+			headers: {
+				'Cache-Control': cacheControl,
+				'content-encoding': 'gzip',
+				'content-type': 'text/html'
+			}
+		})),
+
+		gulp.src([ DEST_GZIP + '/**/*.js' ])
+		.pipe($.s3(aws, {
+			headers: {
+				'Cache-Control': cacheControl,
+				'content-encoding': 'gzip',
+				'content-type': 'application/javascript'
+			}
+		})),
+
+		gulp.src([ DEST_GZIP + '/**/*.css' ])
+		.pipe($.s3(aws, {
+			headers: {
+				'Cache-Control': cacheControl,
+				'content-encoding': 'gzip',
+				'content-type': 'text/css'
+			}
+		})),
+
+		gulp.src([ DEST_GZIP + '/**/*.json' ])
+		.pipe($.s3(aws, {
+			headers: {
+				'Cache-Control': cacheControl,
+				'content-encoding': 'gzip',
+				'content-type': 'application/json'
+			}
+		})),
+
+		gulp.src([ DEST_GZIP + '/**/*.svg' ])
+		.pipe($.s3(aws, {
+			headers: {
+				'Cache-Control': cacheControl,
+				'content-encoding': 'gzip',
+				'content-type': 'image/svg+xml'
+			}
+		})),
+
+		gulp.src([ DEST_GZIP + '/**/*.woff2' ])
+		.pipe($.s3(aws, {
+			headers: {
+				'Cache-Control': cacheControl,
+				'content-encoding': 'gzip',
+				'content-type': 'application/font-woff2'
+			}
+		})),
+
+		gulp.src([
+			DEST_GZIP + '/**/*.*',
+			'!' + DEST_GZIP + '/**/*.DS_Store',
+			'!' + DEST_GZIP + '/**/*.woff2',
+			'!' + DEST_GZIP + '/**/*.html',
+			'!' + DEST_GZIP + '/**/*.js',
+			'!' + DEST_GZIP + '/**/*.css',
+			'!' + DEST_GZIP + '/**/*.json',
+			'!' + DEST_GZIP + '/**/*.svg'
+		])
+		.pipe($.s3(aws, {
+			headers: {
+				'Cache-Control': cacheControl
+			}
+		}))
+	)
+	.pipe($.size({title: 's3'}));
+
+});
+/**
+ * Deploys the code. Asks for confirmation if deploying to production
+ */
+gulp.task('confirm-deploy', [], function () {
+	if(argv.environment == 'production'){
+		return gulp.src('')
+		.pipe($.prompt.confirm('You are about to deploy TO THE LIVE SITE! Are you sure you want to continue)'))
+		.pipe($.prompt.confirm('Really sure?!'))
+	}
+
+});
+gulp.task('deploy', function (cb) {
+	runSequence(
+		'confirm-deploy',
+		's3',
+	cb);
+});
+
+/* Cleaners ----------------------------------------------------------------- */
+/**
+ * Delete all generated files
+ */
+gulp.task('clean:dev', del.bind(null, [DEST_DEV]));
+gulp.task('clean:polymer', del.bind(null, [DEST_POLYMER]));
+gulp.task('clean:gzip', del.bind(null, [DEST_GZIP]));
+/* Builders ----------------------------------------------------------------- */
+gulp.task('build:dev', ['clean:dev'], function (cb) {
+	runSequence(
+		'soft-jshint',
+		'jekyll',
+		'js',
+
+	cb);
+});
+gulp.task('build:polymer', ['clean:polymer'], function (cb) {
+	runSequence(
+		'jshint',
+		'jekyll',
+		'js',
+		'polymer-copy',
+		'polymer-use-minified-paths',
+		'polymer-polybuild',
+		'polymer-clean',
+		'polymer-revision',
+	cb);
+});
+gulp.task('build:gzip', ['clean:gzip'], function (cb) {
+	runSequence(
+		'build:polymer',
+		'gzip-compression',
+	cb);
+});
+
+/* Servers ------------------------------------------------------------------ */
+gulp.task('serve:dev', ['build:dev'], $.serve({
+	root: [DEST_DEV],
+	port: 5500,
+	middleware: function(req, res, next) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	}
+}));
+gulp.task('serve:polymer', ['build:polymer'],  $.serve({
+	root: [DEST_POLYMER],
+	port: 5501,
+	middleware: function(req, res, next) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	}
+}));
+gulp.task('serve:gzip', ['build:gzip'],  $.serve({
+	root: [DEST_GZIP],
+	port: 5502,
+	middleware: function(req, res, next) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+		if(req.url[req.url.length - 1] == '/' || (/\.(html|js|css|json|svg|woff2)/).test(req.url)){
+			res.setHeader('content-encoding', 'gzip');
+
+			if((/\.(html)$/i).test(req.url)){
+				res.setHeader('content-type', 'text/html');
+			}
+			else if((/\.(js)$/i).test(req.url)){
+				res.setHeader('content-type', 'application/javascript');
+			}
+			else if((/\.(css)$/i).test(req.url)){
+				res.setHeader('content-type', 'text/css');
+			}
+			else if((/\.(json)$/i).test(req.url)){
+				res.setHeader('content-type', 'application/json');
+			}
+			else if((/\.(svg)$/i).test(req.url)){
+				res.setHeader('content-type', 'image/svg+xml');
+			}
+			else if((/\.(woff2)$/i).test(req.url)){
+				res.setHeader('content-type', 'application/font-woff2');
+			}
+		}
+		next()
+	}
+}));
+/* Watchers ----------------------------------------------------------------- */
+gulp.task('watch:dev', ['serve:dev'], function() {
+	gulp.watch(SRC + '/**/*', ['build:dev']);
+});
+gulp.task('watch:polymer', ['serve:polymer'], function() {
+	gulp.watch(SRC + '/**/*', ['build:polymer']);
+});
+gulp.task('watch:gzip', ['serve:gzip'], function() {
+	gulp.watch(SRC + '/**/*', ['build:gzip']);
+});
